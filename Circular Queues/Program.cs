ae4e4aa8a1cb5f93a2a858e8bfec9721f1dd9494
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-14 17:05:23 +0700

    Creating a method to display the queue

--- Circular Queues/Program.cs
+++ Circular Queues/Program.cs
@@ -73,7 +73,47 @@ namespace CircularQueues_CSharp
                      of array.*/
                     FRONT = FRONT + 1;
                 }
+            }
+        }
 
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            /*Checks if the queue is empty.*/
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are...............\n");
+            if (FRONT_position <= REAR_position)
+            {
+                /*Traverses the queue till the last element present in an array.*/
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                /*Traverses the queue till the last position of the array.*/
+                while (FRONT_position <= max - 1)
+                {
+                    Console.Write(queue_array[FRONT_position] + "     ");
+                    FRONT_position++;
+                }
+                /*Set the FRONT position to the first element of the array.*/
+                FRONT_position = 0;
+                /*Traverses the array till the last element in the queue*/
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
             }
         }
     }   
