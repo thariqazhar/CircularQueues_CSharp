6db33c234b7ded75409742c529772f65e041d534
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-14 16:49:48 +0700

    Creating a method for inserting element to the queue

--- Circular Queues/Program.cs
+++ Circular Queues/Program.cs
@@ -13,5 +13,35 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            /*This statement checks for the overflow condition*/
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            /*The following statement checks wheter the queue is empty. If the queue is empty,
+             * then the value of the REAR and FRONT variables is set to 0.*/
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /*If REAR is at the last position of the array, then the value of REAR
+                 * is set to 0 that corresponds to the first position of the array.*/
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /*If REAR is not at the last position, then its value is
+                     * incremented by one.*/
+                    REAR = REAR + 1;
+            }
+            /*Once the position of REAR is determined, the element is added at its proper place,*/
+            queue_array[REAR] = element;
+
+        }
     }
 }
