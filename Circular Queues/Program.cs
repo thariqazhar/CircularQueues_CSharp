3f3490204879bd763fc33d89065a98d9b62fc856
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-14 16:56:28 +0700

    Creating a method for removing element from the queue

--- Circular Queues/Program.cs
+++ Circular Queues/Program.cs
@@ -43,5 +43,38 @@ namespace CircularQueues_CSharp
             queue_array[REAR] = element;
 
         }
-    }
+
+        public void remove()
+        {
+            /*Checks whether the queue is empty.*/
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return;
+            }
+            Console.WriteLine("\nThe element deleted from the queue is: " +
+                queue_array[FRONT] + "\n");
+            /*Checks if the queue has one element.*/
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /*If the element to be deleted is at the last position of 
+                 the array, then the value of FRONT is set to 0 i.e
+                 to the first element of the array.*/
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                {
+                    /*FRONT is incremented by one if it's not the first element
+                     of array.*/
+                    FRONT = FRONT + 1;
+                }
+
+            }
+        }
+    }   
 }
