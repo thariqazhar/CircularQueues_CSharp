78a778f526f49ee4ed00f8032003739988e0d0e8
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-14 16:42:14 +0700

    Initializing the values of the variables REAR and FRONT to -1

--- Circular Queues/Program.cs
+++ Circular Queues/Program.cs
@@ -6,5 +6,12 @@ namespace CircularQueues_CSharp
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+        public Queues()
+        {
+            /*Initializing the values of the variables REAR and FRONT to -1 to
+             * indicate that the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 }
